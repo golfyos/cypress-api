751b253a570ba503b0fd60284f01dea91f7b37d7
golfyos <golf_cat1@hotmail.com>
2020-11-04 19:57:32 +0700

    Created command support file

---
+++ cypress/support/commands.ts
@@ -0,0 +1,72 @@
+import { HttpMethod } from '../utils/http-method'
+import { IncomingHttpHeaders } from 'http'
+
+export interface HttpRequestHeader extends IncomingHttpHeaders {}
+
+export enum UserType {
+    ADMIN = 'admin',
+    USER = 'user',
+}
+
+function combineURLs(baseURL: string, relativeURL: string) {
+    return relativeURL ? baseURL.replace(/\/+$/, '') + '/' + relativeURL.replace(/^\/+/, '') : baseURL
+}
+
+Cypress.Commands.add('call', (uri: string, method: HttpMethod, body: any, headers: HttpRequestHeader = {}) => {
+    const baseUrl = Cypress.env('url')
+    const normalizeUrl = combineURLs(baseUrl, uri)
+
+    cy.request({
+        url: normalizeUrl,
+        method,
+        body,
+        headers,
+        encoding: 'utf-8',
+    })
+})
+
+Cypress.Commands.add('graphql', (graphql: string, headers: HttpRequestHeader = {}) => {
+    const baseUrl = Cypress.env('url')
+    const normalizeUrl = combineURLs(baseUrl, '/')
+
+    cy.request({
+        url: normalizeUrl,
+        method: HttpMethod.POST,
+        body: {
+            query: graphql,
+        },
+        headers,
+        encoding: 'utf-8',
+    })
+})
+
+Cypress.Commands.add('getAccessToken', (username: string, password: string) => {
+    return cy
+        .request({
+            url: Cypress.env('url'),
+            method: HttpMethod.POST,
+            body: {
+                query: `
+                query {
+                    userLogin (
+                        email: "${username}"
+                        password: "${password}"
+                    ) {
+                        tokenType
+                        accessToken
+                        refreshToken
+                        expiresIn
+                    }
+                }
+            `,
+            },
+        })
+        .then((response: Cypress.Response) => {
+            const tokenType = response.body?.data?.userLogin?.tokenType
+            const accessToken = response.body?.data?.userLogin?.accessToken
+
+            return {
+                token: `${tokenType} ${accessToken}`,
+            }
+        })
+})
