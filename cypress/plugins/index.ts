fa633078f9124e6fd11efdd9158c282c9a5e3c20
golfyos <golf_cat1@hotmail.com>
2020-11-21 16:29:50 +0700

    Updated plugin

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -3,5 +3,14 @@
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on: any, config: Cypress.ConfigOptions) => {
+    const env = config.env.environment
+    const url = config.env.url
+
+    if (env === 'integration') {
+        config.baseUrl ??= url
+    } else {
+        config.baseUrl ??= `http://${env}-${url}`
+    }
+
     return config
 }
